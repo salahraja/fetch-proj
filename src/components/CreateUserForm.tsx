b3847d6f17c7eda7849691a4ebae186bbf4646cc
Salah Uddin <salah7252@icloud.com>
2023-03-07 03:42:24 +0000

    fixed import and function UserForm

--- src/components/CreateUserForm.tsx
+++ src/components/CreateUserForm.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { FormEvent, SyntheticEvent, useEffect, useState } from "react";
+import React, { FormEvent, useEffect, useState } from "react";
 
 type Occupation = string;
 interface State {
@@ -28,19 +28,12 @@ interface APIGetResponse {
 // interface APIPostResponse
 
 export default function UserForm() {
-  const [formData, setFormData] = useState<FormData>({
-    "full-name": "",
-    email: "",
-    password: "",
-    occupation: "",
-    state: "",
-  });
   const [apiData, setAPIData] = useState<APIGetResponse>({
     occupations: [],
     states: [],
   });
-  const [createdUser, setCreatedUser] = useState<User | null>(null);
   const [error, setError] = useState<string | null>(null);
+  const [isSuccessful, setIsSuccessful] = useState(false);
 
   useEffect(() => {
     fetch("https://frontend-take-home.fetchrewards.com/form")
@@ -56,59 +49,77 @@ export default function UserForm() {
   function handleSubmit(event: FormEvent<HTMLFormElement>): void {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    const fullName = data.get("fullName");
+    const name = data.get("name");
     const email = data.get("email");
     const password = data.get("password");
     const occupation = data.get("occupation");
     const state = data.get("state");
-    const body = { fullName, email, password, occupation, state };
+    const body = { name, email, password, occupation, state };
+
+    setError("");
+    setIsSuccessful(false);
 
     fetch("https://frontend-take-home.fetchrewards.com/form", {
       method: "POST",
+      // important, fails without content type header set to application/json
+      headers: {
+        "Content-Type": "application/json",
+      },
       body: JSON.stringify(body),
     })
-      .then((response) => response.json())
+      .then((response) => {
+        if (response.status === 400) {
+          throw new Error("Invalid submission");
+        } else if (response.status !== 201) {
+          throw new Error("Unknown error");
+        }
+
+        setIsSuccessful(true);
+      })
       .catch((error) => {
         setError(error.message);
       });
   }
 
-  console.log(apiData);
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="form">
-        <label htmlFor="fullName">Full Name</label>
-        <input type="text" name="fullName" id="fullName" />
-      </div>
+    <>
+      <>{error && <div className="error">{error}</div>}</>
+      <>{isSuccessful && <div className="success">Form submitted, thank you.</div>}</>
+      <form onSubmit={handleSubmit}>
+        <div className="form">
+          <label htmlFor="name">Full Name</label>
+          <input type="text" name="name" id="name" required />
+        </div>
 
-      <div className="form">
-        <label htmlFor="email">Email</label>
-        <input type="email" name="email" id="email" />
-      </div>
-      <div className="form">
-        <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password" />
-      </div>
-      <div className="form">
-        <label htmlFor="occupation">Occupation</label>
-        <select name="occupation" id="occupation">
-          {apiData.occupations.map((occupation, index) => (
-            <option key={index}>{occupation}</option>
-          ))}
-        </select>
-      </div>
-      <div className="form">
-        <label htmlFor="state">State</label>
-        <select name="state" id="state">
-          {apiData.states.map((state, index) => (
-            <option value={state.abbreviation} key={index}>
-              {state.name}
-            </option>
-          ))}
-        </select>
-      </div>
+        <div className="form">
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" id="email" required />
+        </div>
+        <div className="form">
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="password" required />
+        </div>
+        <div className="form">
+          <label htmlFor="occupation">Occupation</label>
+          <select name="occupation" id="occupation" required>
+            {apiData.occupations.map((occupation, index) => (
+              <option key={index}>{occupation}</option>
+            ))}
+          </select>
+        </div>
+        <div className="form">
+          <label htmlFor="state">State</label>
+          <select name="state" id="state" required>
+            {apiData.states.map((state, index) => (
+              <option value={state.abbreviation} key={index}>
+                {state.name}
+              </option>
+            ))}
+          </select>
+        </div>
 
-      <button type="submit">Submit</button>
-    </form>
+        <button type="submit">Submit</button>
+      </form>
+    </>
   );
 }
